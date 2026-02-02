70fcf23f606bf917e885963433db8984d54a490c
richlue <richluekingwork@gmail.com>
2026-02-01 20:40:58 -0800

    clearnup 5

--- clrproj/settings.py
+++ clrproj/settings.py
@@ -28,9 +28,9 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.railway.app,localhost,127.0.0.1', cast=Csv())
+#ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='.railway.app,localhost,127.0.0.1', cast=Csv())
 # CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS', default='http://127.0.0.1', cast=Csv())
-
+ALLOWED_HOSTS = ['*']  # ← only for debugging — don't leave this in prod!
 
 # Application definition
 
